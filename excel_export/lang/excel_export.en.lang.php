cd6a4368491a6ae6594c62406bf792630d13c540
sodium carbonate <webitproff@gmail.com>
2026-02-02 18:04:56 +0200

    Update excel_export.en.lang.php

--- excel_export/lang/excel_export.en.lang.php
+++ excel_export/lang/excel_export.en.lang.php
@@ -1,19 +1,34 @@
 <?php
+/**
+ * Tool for exporting data from any table of the Cotonti CMF database to Excel
+ * Plugin excel_export for Cotonti 0.9.26, PHP 8.4+
+ * Filename: excel_export.en.lang.php
+ * Purpose: English localization strings for the Plugin excel_export
+ * Date: Feb 02nd, 2026
+ * @package excel_export
+ * @version 2.0.1
+ * @author webitproff
+ * @copyright Copyright (c) webitproff 2026 | https://github.com/webitproff
+ * @license BSD
+ */
+
 defined('COT_CODE') or die('Wrong URL');
 
-$L['cfg_export_table'] = 'Target Table';
-$L['cfg_export_table_hint'] = 'Specify <code>pages</code> to export data, e.g., from cot_pages';
-$L['cfg_max_rows'] = 'Maximum Rows';
-$L['cfg_max_rows_hint'] = 'Maximum number of rows to export (0 — no limit)';
+$L['cfg_export_table'] = 'Target table';
+$L['cfg_export_table_hint'] = 'Specify the table name <strong>without prefix</strong>, for example <code>pages</code> to export data from the <strong>cot_pages</strong> table';
+$L['cfg_max_rows'] = 'Maximum rows';
+$L['cfg_max_rows_hint'] = 'Maximum number of rows to export. <br>Set to 0 to export without row limits.';
+
 $L['info_title'] = 'Export to Excel via PhpSpreadsheet';
-$L['info_desc'] = 'Tool for exporting data from Cotonti tables to Excel';
-$L['info_notes'] = 'Uses PhpSpreadsheet library version 1.23.0 without Composer. Tested on Cotonti 0.9.26 with PHP 8.2';
+$L['info_desc'] = 'Tool for exporting data from any Cotonti CMF database table to Excel';
+$L['info_notes'] = 'Uses PhpSpreadsheet library version 1.23.0 without Composer. Tested on Cotonti 0.9.26 with PHP 8.4 & MySQL 8.0';
 
-$L['excel_export_title'] = 'Export to Excel via PhpSpreadsheet';
-$L['excel_export_subtitle'] = 'Tool for exporting data from Cotonti tables to Excel';
+$L['excel_export_title'] = $L['info_title'];
+$L['excel_export_subtitle'] = $L['info_desc'];
 
-$L['excel_export_field_table'] = 'Database Field';
+$L['excel_export_field_table'] = 'Database field';
 $L['excel_export_select'] = 'Select';
-$L['excel_export_field_excel'] = 'Excel Column Name';
-$L['excel_export_max_rows_label'] = 'Maximum Number of Rows';
+$L['excel_export_field_excel'] = 'Excel column name';
+$L['excel_export_max_rows_label'] = 'Maximum number of rows';
 $L['excel_export_export'] = 'Export';
+$L['excel_export_export_info'] = 'After clicking the export button, nothing may appear to happen for a second or two, depending on the data volume. This is normal — please wait until the export process completes.';
